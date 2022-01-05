a7301e5f6a7d1f5a3c1f2306f230072cd73f6808
kautzz <5493156+kautzz@users.noreply.github.com>
2022-01-06 00:52:40 +0100

    d

--- run.sh
+++ run.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-# Create a new session named "$sessname", and run command
-tmux new-session -d -s "main"
-
 while true; do
-  tmux send-keys -t "main" "python3 c14garden.py" Enter
-  tmux attach -t "main"
+  tmux new-session -d -s "log"
+  tmux send-keys -t "log" "python3 c14garden.py" Enter
+  tmux attach -t "log"
+  tmux kill-session -t "log"
   #python3 c14garden.py
   sleep 10
 done
