f8f245e3184cbf5fdb085d5b07142f59e4de9ea0
kautzz <5493156+kautzz@users.noreply.github.com>
2022-01-05 22:15:32 +0100

    sensor unblock

--- sensors.py
+++ sensors.py
@@ -20,24 +20,27 @@ def setup_bme():
     bme.set_pressure_oversample(bme680.OS_4X)
     bme.set_temperature_oversample(bme680.OS_8X)
     bme.set_filter(bme680.FILTER_SIZE_3)
-    bme.set_gas_status(bme680.ENABLE_GAS_MEAS)
     bme.set_gas_heater_temperature(320)
     bme.set_gas_heater_duration(150)
     bme.select_gas_heater_profile(0)
+    bme.set_gas_status(bme680.ENABLE_GAS_MEAS)
+
 
 def read():
     if bme.get_sensor_data():
+        readings = {
+            "sensor": "BME680",
+            "temperature": bme.data.temperature,
+            "humidity": bme.data.humidity,
+            "pressure": bme.data.pressure,
+            "gas_resistance": 0
+        }
+
         if bme.data.heat_stable:
-            readings = {
-                "sensor": "BME680",
-                "temperature": bme.data.temperature,
-                "humidity": bme.data.humidity,
-                "pressure": bme.data.pressure,
-                "gas_resistance": bme.data.gas_resistance
-            }
-            send(readings)
-            return(readings)
-    return(False)
+            readings.gas_resistance = bme.data.gas_resistance
+
+    send(readings)
+    return(readings)
 
 def send(readings):
     if readings:
