5f088bb588973c61058b269dc70acdcf253d51ad
kautzz <5493156+kautzz@users.noreply.github.com>
2022-01-06 01:01:43 +0100

    fu

--- run.sh
+++ run.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
-tmux new-session -d -s "log"
-#tmux send-keys -t "log" "python3 c14garden.py" Enter
-
 while true; do
-  tmux attach -t "log"
   python3 c14garden.py
-  tmux kill-session -t "log"
-
   sleep 10
 done
 exit
