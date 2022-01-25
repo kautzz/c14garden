223982e8d3d5ab5434f5f35d5cb12d4a5b6388b5
kautzz <5493156+kautzz@users.noreply.github.com>
2022-01-25 03:21:31 +0100

    s

--- actuators.py
+++ actuators.py
@@ -51,13 +51,13 @@ class RelayBoard(object):
         time = datetime.today().strftime('%H:%M')
         for interval in self.schedule:
 
-            if list(interval.keys())[0] == day and interval[day] in time:
+            if list(interval.keys())[0] == day and interval[day] == time:
                 if self.schedule_active == False:
                     self.schedule_active = True
                     self.activate()
 
-            elif (list(interval.keys())[0] == day
-                and interval[day] not in time
+            if (list(interval.keys())[0] == day
+                and interval[day] != time
                 and self.schedule_active == True):
                 self.schedule_active = False
                 self.deactivate()
