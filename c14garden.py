d540a389879699415859c2d2ee8e1b4d25477687
kautzz <5493156+kautzz@users.noreply.github.com>
2022-01-28 19:31:41 +0100

    testing external naming of mqtt device channel

--- c14garden.py
+++ c14garden.py
@@ -30,7 +30,7 @@ def main():
 
     try:
         client.connect("192.168.1.100",1883,60)
-        client.publish("growbed1/system", '{"sysmsg": "script start"}')
+        client.publish(config['device']['name'] + "/system", '{"sysmsg": "script start"}')
         client.disconnect()
 
     except Exception as e:
